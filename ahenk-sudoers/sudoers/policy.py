df2cbfe6dc898ea92025ad23bf60eaa852564ea8
Volkan Şahin <bm.volkansahin@gmail.com>
2016-10-07 11:21:20 +0300

    log fixing

--- ahenk-sudoers/sudoers/policy.py
+++ ahenk-sudoers/sudoers/policy.py
@@ -24,34 +24,37 @@ class GrantSudoAccess(AbstractPlugin):
 
                 if str(json_data['privilege']) == 'True':
                     self.execute('adduser {} sudo'.format(username))
-                    self.logger.debug('[Sudoers]User sudoers set privilege to {}.'.format(username))
+                    self.logger.debug('User sudoers set privilege to {}.'.format(username))
                     # TODO command instead reboot
 
-                    self.logger.debug('[Sudoers] Creating response...')
-                    self.context.create_response(self.get_message_code().POLICY_PROCESSED, 'User sudoers set privilege to {} successfully.'.format(username))
+                    self.logger.debug('Creating response...')
+                    self.context.create_response(self.get_message_code().POLICY_PROCESSED,
+                                                 'User sudoers set privilege to {} successfully.'.format(username))
 
                 elif str(json_data['privilege']) == 'False':
                     self.execute('deluser {} sudo'.format(username))
                     self.logger.debug('[Sudoers]User sudoers removed privilege from {}.'.format(username))
                     # TODO command instead reboot
 
-                    self.logger.debug('[Sudoers] Creating response...')
-                    self.context.create_response(self.get_message_code().POLICY_PROCESSED, 'User sudoers removed privilege from {} successfully.'.format(username))
+                    self.logger.debug('Creating response...')
+                    self.context.create_response(self.get_message_code().POLICY_PROCESSED,
+                                                 'User sudoers removed privilege from {} successfully.'.format(
+                                                     username))
 
                 else:
                     self.context.create_response(self.get_message_code().POLICY_PROCESSED, 'Missing parameter error.')
 
-                self.logger.debug('[Sudoers] Sudoers profile is handled successfully.')
+                self.logger.debug('Sudoers profile is handled successfully.')
             else:
-                self.logger.error('[Sudoers] Username parameter is missing.')
+                self.logger.error('Username parameter is missing.')
                 self.context.create_response(self.get_message_code().POLICY_ERROR, 'Username is missing')
 
         except Exception as e:
-            self.logger.error('[Sudoers] A problem occurred while handling sudoers profile: {0}'.format(str(e)))
-            self.context.create_response(self.get_message_code().POLICY_ERROR, 'A problem occurred while handling sudoers profile: {0}'.format(str(e)))
+            self.logger.error('A problem occurred while handling sudoers profile: {0}'.format(str(e)))
+            self.context.create_response(self.get_message_code().POLICY_ERROR,
+                                         'A problem occurred while handling sudoers profile: {0}'.format(str(e)))
 
 
 def handle_policy(profile_data, context):
-    print('GRANT-SUDO-ACCESS')
     quota = GrantSudoAccess(profile_data, context)
     quota.handle_policy()
