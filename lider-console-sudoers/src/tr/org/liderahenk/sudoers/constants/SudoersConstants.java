a99c871ac0fe3675f1632c2644198da7f9479ff2
tncyclk <tuncay.colak@tubitak.gov.tr>
2017-01-27 15:47:13 +0300

    fix 1.0.0-beta version

--- lider-console-sudoers/src/tr/org/liderahenk/sudoers/constants/SudoersConstants.java
+++ lider-console-sudoers/src/tr/org/liderahenk/sudoers/constants/SudoersConstants.java
@@ -4,7 +4,7 @@ public class SudoersConstants {
 	
 	public static final String PLUGIN_NAME = "sudoers";
 	
-	public static final String PLUGIN_VERSION = "1.0.0";
+	public static final String PLUGIN_VERSION = "1.0.0-beta";
 	
 	public static final class PARAMETERS {
 		public static final String PRIVILEGE = "privilege";
